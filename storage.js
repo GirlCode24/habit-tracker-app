5b304f67474b1764b535d8d512df8ca878cfd6fa
GirlCode24 <janetquayenaa@gmail.com>
2025-05-07 13:25:19 +0000

    Set up basic structure for habit tracker

---
+++ storage.js
@@ -0,0 +1,5 @@
+// let habits = JSON.parse(localStorage.getItem('habits')) || [];
+// let currentDate = new Date().toISOString().split('T')[0];
+// function saveHabits() {
+//     localStorage.setItem('habits', JSON.stringify(habits));
+// }
